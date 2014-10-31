d12d97449ea28013d3a0b657948f3e9350ef644e
Pavel Lasarev <paullasarev@gmail.com>
2014-10-31 13:47:13 +0400

    fix deferred resolve

--- promise.js
+++ promise.js
@@ -1,12 +1,20 @@
 function Promise(worker) {
   this.onResolve = undefined;
+  this.result = undefined;
+  this.state = 'pending';
 
   this.resolve = function(result) {
-    this.onResolve(result);
+    this.result = result;
+    this.state = 'resolved';
+    if (this.onResolve)
+      this.onResolve(result);
   }
 
   this.then = function(onResolve) {
-    this.onResolve = onResolve;
+    if (this.state === 'resolved')
+      onResolve(this.result);
+    else if (this.state === 'pending')
+      this.onResolve = onResolve;
   }
 
   worker(this.resolve.bind(this));
