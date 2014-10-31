ac8324ffb09ac13cb060085c78fa0a0c796b053c
Pavel Lasarev <paullasarev@gmail.com>
2014-10-31 14:03:59 +0400

    use prototype methods

--- promise.js
+++ promise.js
@@ -4,35 +4,34 @@ function Promise(worker) {
   this.result = undefined;
   this.state = 'pending';
 
-  this.resolve = function(result) {
-    this.result = result;
-    this.state = 'resolved';
-    if (this.onResolve)
-      this.onResolve(result);
-  }
-
-  this.reject = function(result) {
-    this.result = result;
-    this.state = 'rejected';
-    if (this.onReject)
-      this.onReject(result);
-  }
-
-  this.then = function(onResolve, onReject) {
-    if (this.state === 'resolved')
-      onResolve(this.result);
-    else if (this.state === 'rejected')
-      onReject(this.result);
-    else if (this.state === 'pending')
-    {
-      this.onResolve = onResolve;
-      this.onReject = onReject;
-    }
-  }
-
   worker(this.resolve.bind(this), this.reject.bind(this));
 }
 
+Promise.prototype.resolve = function(result) {
+  this.result = result;
+  this.state = 'resolved';
+  if (this.onResolve)
+    this.onResolve(result);
+}
+
+Promise.prototype.reject = function(result) {
+  this.result = result;
+  this.state = 'rejected';
+  if (this.onReject)
+    this.onReject(result);
+}
+
+Promise.prototype.then = function(onResolve, onReject) {
+  if (this.state === 'resolved')
+    onResolve(this.result);
+  else if (this.state === 'rejected')
+    onReject(this.result);
+  else if (this.state === 'pending')
+  {
+    this.onResolve = onResolve;
+    this.onReject = onReject;
+  }
+}
 
 module.exports = function(worker) {
   return new Promise(worker);
